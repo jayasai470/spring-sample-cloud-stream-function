b4dec97478de58ff3f0d9315739436202f138646
Jaya Sai <j.muppala@partner.samsung.com>
2019-03-29 10:55:54 -0500

    update to latest

--- src/main/java/com/example/streamfunction/streamfunction/StreamFunctionApplication.java
+++ src/main/java/com/example/streamfunction/streamfunction/StreamFunctionApplication.java
@@ -1,36 +1,33 @@
 package com.example.streamfunction.streamfunction;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
 
 import java.util.function.Consumer;
 
 @SpringBootApplication
-@EnableBinding(Sink.class)
+@EnableBinding(Source.class)
 public class StreamFunctionApplication {
-
-	@Autowired
-	private Sink sink;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StreamFunctionApplication.class, "--spring.cloud.stream.function.definition=storeSync");
 	}
 
-	@Bean
-	public Consumer<String> storeSync() {
-		return this::store;
-	}
 
-	public boolean store(String paylaod){
-	    System.out.println(paylaod);
-        boolean send = this.sink.input().send(MessageBuilder.withPayload(paylaod).build());
-        System.out.println(send);
-		return send;
-	}
+    @Bean
+    public Consumer<String> storeSync(ApplicationContext context) {
+        return v -> {
+            System.out.println("payload is " + v);
+            MessageChannel channel = context.getBean(Source.OUTPUT, MessageChannel.class);
+            boolean send = channel.send(MessageBuilder.withPayload(v).build());
+            System.out.println("send data " + send);
+        };
+    }
 
 }
